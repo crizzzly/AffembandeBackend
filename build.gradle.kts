4ceb09f599c07e83ccfcdb2a6b5f9d7826117ef9
crizzzly <c.christina.rost@gmail.com>
2024-10-22 13:45:41 +0200

    bugfixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-jdbc:2.7.0")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	implementation("org.springframework.boot:spring-boot-starter-logging")
-	implementation("io.springfox:springfox-boot-starter:3.0.0")
+//	implementation("io.springfox:springfox-boot-starter:3.0.0")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa") // JPA with Hibernate
