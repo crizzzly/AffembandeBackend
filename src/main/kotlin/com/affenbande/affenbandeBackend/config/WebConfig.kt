e153594f9e93991c8596d3a74597732c3a0bc000
crizzzly <c.christina.rost@gmail.com>
2025-01-05 22:06:38 +0100

    server configs

--- src/main/kotlin/com/affenbande/affenbandeBackend/config/WebConfig.kt
+++ src/main/kotlin/com/affenbande/affenbandeBackend/config/WebConfig.kt
@@ -21,14 +21,12 @@ class WebConfig {
     @Bean
     fun corsConfigurer(): WebMvcConfigurer {
         return object : WebMvcConfigurer {
-            val host = System.getenv("DB_HOST")
-            val port = System.getenv("DB_PORT")
-            val url = "http://${host}:${port}"
+
             override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/api/**") // Adjust the path as needed
+                registry.addMapping("/**") // Adjust the path as needed
                     .allowedOrigins("*") // use Nginx as proxy for production
                     .allowedMethods("GET", "POST", "DELETE")
-                    .allowedHeaders("Content-Type", "Authorization") // Specific headers
+                    .allowedHeaders("*") // Specific headers
                     .maxAge(3600)
                     // .allowCredentials(true) // Only if needed, with CSRF protection
             }
